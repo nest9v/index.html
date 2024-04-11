2a4bcb20013c2b100eec8de88e8c3efcb37d08aa
anansgay <166581210+anansgay@users.noreply.github.com>
2024-04-11 16:47:54 +0000

    hg

--- at.js
+++ at.js
@@ -46,3 +46,17 @@ document.addEventListener('DOMContentLoaded', function () {
       closeM();
     }
   }
+
+
+  const wrapper = document.querySelector('.wrapper');
+  const loginLink = document.querySelector('.login-link');
+  const registerLink = document.querySelector('.register-link');
+  const iconClose = document.querySelector('.icon-close');
+
+  registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+  });
+
+  loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+  });
