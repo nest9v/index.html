7d6b50b71082293da6bdfb2eff6bf5e6b332cd70
anansgay <166581210+anansgay@users.noreply.github.com>
2024-04-17 04:40:52 +0000

    cdc

--- at.js
+++ at.js
@@ -1,43 +1,6 @@
 
   
-  function openModal(){
-    let modal= document.querySelector('#modal-window');
-    modal.classList.add("showModal");
-  }
   
-  function closeM(){
-      let m= document.querySelector('#modal-window');
-    m.classList.remove("showModal");
-  }
-  
-  
-  
-  const cardItems = document.querySelectorAll('.main-card');
-  const modalHeader = document.querySelector('.modalHeader-js');
-  const modalCardPrice = document.querySelector('.amount');
-  
-  cardItems.forEach((cardItem) => {
-    cardItem.addEventListener('click', function () {
-      const cardHeader = cardItem.querySelector('.cardText-js');
-      const cardPrice = cardItem.querySelector('.card-price');
-  
-      modalHeader.innerText = cardHeader.innerText;
-      modalCardPrice.innerText = cardPrice.innerText;
-    });
-  });
-  
-  window.onkeydown = function (event) {
-    if(event.keyCode == 27) {
-      closeM();
-    }
-  }
-  
-  var modal =  document.querySelector('#modal-window');
-  window.onclick = function (event) {
-    if(event.target == modal) {
-      closeM();
-    }
-  }
 
 
   const wrapper = document.querySelector('.wrapper');
