df06b1af6d7a6c4fbe478f1afeccc3676c0486e3
nest9v <145523646+nest9v@users.noreply.github.com>
2024-04-20 14:37:15 +0000

    g

--- at.js
+++ at.js
@@ -3,15 +3,16 @@
   
 
 
-  const wrapper = document.querySelector('.wrapper');
-  const loginLink = document.querySelector('.login-link');
-  const registerLink = document.querySelector('.register-link');
-  const iconClose = document.querySelector('.icon-close');
-
-  registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-  });
-
-  loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-  });
+  let wrapper = document.querySelector('.wrapper');
+  let loginLink = document.querySelector('.login-link');
+   let registerLink = document.querySelector('.register-link');
+  let iconClose = document.querySelector('.icon-close');
+ 
+   registerLink.addEventListener('click', ()=> {
+     wrapper.classList.add('active');
+   });
+ 
+   loginLink.addEventListener('click', ()=> {
+     wrapper.classList.remove('active');
+   });
+ 
